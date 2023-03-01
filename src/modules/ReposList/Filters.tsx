4460c0b647d56100de4c008dc6313aded9e0b2d6
Carlos Gomes <carlos.algms@gmail.com>
2023-03-01 14:45:00 +0100

    Adding the filter structure to allow filtering by language

---
+++ src/modules/ReposList/Filters.tsx
@@ -0,0 +1,9 @@
+import styles from './Filters.module.scss';
+
+export default function Filters() {
+  return (
+    <div className={styles.root}>
+      <span>filter?</span>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { useTrendingRepos } from '../modules/github-client';
+import Filters from '../modules/ReposList/Filters';
 import ReposList from '../modules/ReposList/ReposList';
 
 export default function Home() {
@@ -19,11 +20,15 @@ export default function Home() {
 }
 
 function ListWithState() {
-  const { isLoading, error, data = [] } = useTrendingRepos();
+  const { isLoading, error, data = [], isFetched } = useTrendingRepos();
 
-  if (error || isLoading) {
+  if (error || isLoading || !isFetched) {
     return null;
   }
 
-  return <ReposList repos={data} />;
+  return (
+    <ReposList repos={data}>
+      <Filters />
+    </ReposList>
+  );
 }
