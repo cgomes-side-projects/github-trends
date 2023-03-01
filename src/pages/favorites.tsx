1309fe207b3d479a59867f05d5105a5e7bff696e
Carlos Gomes <carlos.algms@gmail.com>
2023-03-01 10:56:11 +0100

    Created the Favorites page using localStorage Data

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import ReposList from '../modules/ReposList/ReposList';
+import { useFavoriteStore } from '../modules/Store/FavoritesStore';
+
+import styles from '../styles/favoritesPage.module.scss';
 
 export default function favorites() {
   return (
@@ -10,7 +15,25 @@ export default function favorites() {
 
       <div className="container">
         <h1>♥️ Favorites</h1>
+        <ListWithState />
       </div>
     </>
   );
+}
+
+function ListWithState() {
+  const storedRepos = useFavoriteStore((state) => Object.values(state.storedRepos));
+
+  if (!storedRepos.length) {
+    return (
+      <section className={styles.noFavorites}>
+        <h3>You have no Favorites yet.</h3>
+        <p>
+          Go to our <Link href="/">Trends</Link> page and start adding some!
+        </p>
+      </section>
+    );
+  }
+
+  return <ReposList repos={storedRepos} />;
 }
